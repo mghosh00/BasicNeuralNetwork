76a0b3ffd48748c116c761879c3152e189a318b4
Matthew Ghosh <matthew.ghosh88@gmail.com>
2024-03-25 21:48:36 +0000

    Changed __init__.py

--- pyNeuralNetwork/neural_network/__init__.py
+++ pyNeuralNetwork/neural_network/__init__.py
@@ -4,29 +4,17 @@ from neural_network import data_generators
 from neural_network import components
 from neural_network import learning
 
-from util import Partitioner
-from util import WeightedPartitioner
-from util import DataSplitter
+# Choose specific classes to expose in the neural_network namespace
+# These are classes that will be accessed by the user
 
-from functions import AbstractFunction
-from functions import ReLU
-from functions import Sigmoid
-from functions import TransferFunction
-from functions import Softmax
-from functions import CrossEntropyLoss
-from functions import MSELoss
+from .util.data_splitter import DataSplitter
 
-from data_generators import AbstractDataGenerator
-from data_generators import NormalDataGenerator
-from data_generators import UniformDataGenerator
+from .data_generators.normal_data_generator import NormalDataGenerator
+from .data_generators.uniform_data_generator import UniformDataGenerator
 
-from components import Neuron
-from components import Edge
-from components import Layer
-from components import Network
+from .components.network import Network
 
-from learning import Plotter
-from learning import AbstractLearner
-from learning import Validator
-from learning import Trainer
-from learning import Tester
+from .learning.plotter import Plotter
+from .learning.validator import Validator
+from .learning.trainer import Trainer
+from .learning.tester import Tester
