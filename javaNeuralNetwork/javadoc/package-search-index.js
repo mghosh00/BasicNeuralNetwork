09271126d7c761a579b857048b5e75fe367d4c0e
mghosh00 <133675135+mghosh00@users.noreply.github.com>
2024-03-27 16:11:46 +0000

    Delete javaNeuralNetwork/javadoc directory

--- javaNeuralNetwork/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"javaNeuralNetwork.main","l":"mvg.neural_network.components"}];updateSearchResults();
--- javaNeuralNetwork/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"mvg.neural_network.components","l":"Neuron"}];updateSearchResults();
