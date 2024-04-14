22e726b598f0f6957837b4163ffb52c11714f6e5
Matthew Ghosh <matthew.ghosh88@gmail.com>
2024-04-14 16:41:29 +0100

    Added regression example

---
+++ javaNeuralNetwork/src/main/java/examples/regression/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains the different regression examples.
+ * <p>
+ * Regression problems arise when each datapoint is labelled by a number on a continuous
+ * scale, and the network tries to learn the patterns of datapoint values.
+ * </p>
+ *
+ * @since 1.0.0
+ * @author mghosh00
+ * @version 1.0.0
+ */
+package examples.regression;
